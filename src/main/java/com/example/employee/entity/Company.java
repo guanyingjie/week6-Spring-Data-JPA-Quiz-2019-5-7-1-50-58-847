9fe45a199d383d5ae37f22cb441e6822613df64d
guanyingjie <39229309+guanyingjie@users.noreply.github.com>
2019-06-11 16:27:24 +0800

    finish emtity

--- src/main/java/com/example/employee/entity/Company.java
+++ src/main/java/com/example/employee/entity/Company.java
@@ -1,5 +1,42 @@
 package com.example.employee.entity;
 
-public class Company {
+import javax.persistence.*;
+@Entity
+@Table(name = "Company")
 
+public class Company {
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "companyName")
+    private String companyName;
+
+    @Column(name = "employeesNumber")
+    private int employeesNumber;
+
+    public Company(int id,String companyName,int employeesNumber){
+        this.id=id;
+        this.companyName=companyName;
+        this.employeesNumber=employeesNumber;
+    }
+    public int getId(){
+        return id;
+    }
+    public String getCompanyName(){
+        return companyName;
+    }
+    public int getEmployeesNumber(){
+        return employeesNumber;
+    }
+    public void setId(int id){
+        this.id=id;
+    }
+    public void setCompanyName(String companyName){
+        this.companyName = companyName;
+    }
+    public void setEmployeesNumber(int employeesNumber){
+        this.employeesNumber=employeesNumber;
+    }
 }
